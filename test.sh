14d4a1560e2e0085bdd1675041aa70b212c60674
Shaleen Garg <shaleengarg.in@gmail.com>
2024-12-04 06:56:07 -0500

    test strace

--- test.sh
+++ test.sh
@@ -4,12 +4,10 @@ set -x
 
 sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"; sleep 3
 
-#COMMAND="./build_release/build/bin/ucsb_bench -db rocksdb  -cfg \"./bench/configs/rocksdb/100GB.cfg\" -wl \"./bench/workloads/100GB.json\" -md \"./db_main/rocksdb/100GB/\" -sd \"/users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/\" -res \"./bench/results/cores_16/disks_1/rocksdb/100GB.json\" -th 16 -fl Read -ri 0 -rc 1"
-COMMAND="./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/100GB.json -md ./db_main/rocksdb/100GB/ -sd /users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/ -res ./bench/results/cores_16/disks_1/rocksdb/100GB.json -th 16 -fl Read -ri 0 -rc 1"
-
+COMMAND="./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/100GB.json -md ./db_main/rocksdb/100GB/ -sd $PWD/../data/rocksdb/100GB/ -res ./bench/results/cores_16/disks_1/rocksdb/100GB.json -th 16 -fl Read -ri 0 -rc 1"
 
 #LD_PRELOAD=/users/gargsaab/ssd/fast_mongodb/shared_lib/lib_speedyio_bookkeeping.so $COMMAND
 
 /usr/bin/time -v $COMMAND
 
-
+#strace -f -tt -o o $PWD/strace.txt $COMMAND
