59af01633479d9ddf7d6e8d6ea6ced6066d6b948
gargsaab <gargsaab@node-0.r6525.smartos-pg0.clemson.cloudlab.us>
2024-12-04 06:28:33 -0500

    test update

--- test.sh
+++ test.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"
+set -x
 
-sleep 3
+sudo sh -c "echo 3 > /proc/sys/vm/drop_caches"; sleep 3
 
-#LD_PRELOAD=/users/gargsaab/ssd/fast_mongodb/shared_lib/lib_speedyio_bookkeeping.so ./build_release/build/bin/ucsb_bench -db rocksdb  -cfg "./bench/configs/rocksdb/default.cfg" -wl "./bench/workloads/100GB.json" -md "./db_main/rocksdb/100GB/" -sd "/users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/" -res "./bench/results/cores_16/disks_1/rocksdb/100GB.json" -th 16 -fl Read -ri 0 -rc 1
+#COMMAND="./build_release/build/bin/ucsb_bench -db rocksdb  -cfg \"./bench/configs/rocksdb/100GB.cfg\" -wl \"./bench/workloads/100GB.json\" -md \"./db_main/rocksdb/100GB/\" -sd \"/users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/\" -res \"./bench/results/cores_16/disks_1/rocksdb/100GB.json\" -th 16 -fl Read -ri 0 -rc 1"
+COMMAND="./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/100GB.json -md ./db_main/rocksdb/100GB/ -sd /users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/ -res ./bench/results/cores_16/disks_1/rocksdb/100GB.json -th 16 -fl Read -ri 0 -rc 1"
 
 
-strace -f -tt -o /users/gargsaab/ssd/SHALEEN/ucsb/strace_ucsb/strace_long_slow_zipfian_read.txt ./build_release/build/bin/ucsb_bench -db rocksdb  -cfg "./bench/configs/rocksdb/default.cfg" -wl "./bench/workloads/100GB.json" -md "./db_main/rocksdb/100GB/" -sd "/users/gargsaab/ssd/SHALEEN/ucsb/../data/rocksdb/100GB/" -res "./bench/results/cores_16/disks_1/rocksdb/100GB.json" -th 16 -fl Read -ri 0 -rc 1
+#LD_PRELOAD=/users/gargsaab/ssd/fast_mongodb/shared_lib/lib_speedyio_bookkeeping.so $COMMAND
+
+/usr/bin/time -v $COMMAND
+
+
