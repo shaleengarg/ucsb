1b031e78d85a6eaae2fbec2539fd247b0439f4f8
gargsaab <gargsaab@node-0.r650.smartos-pg0.clemson.cloudlab.us>
2024-12-24 22:41:56 -0500

    250GB workload

--- load.sh
+++ load.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-THREADS=16
-DATA_FOLDER="$HOME/slowssd/data/rocksdb/500GB"
+SIZE="250GB"
 
-./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/500GB.json -md ./db_main/rocksdb/500GB/ -sd $DATA_FOLDER -res ./bench/results/cores_16/disks_1/rocksdb/500GB.json -th $THREADS -fl Init -ri 0 -rc 1
+THREADS=16
+DATA_FOLDER="$HOME/slowssd/data/rocksdb/${SIZE}"
+
+./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/${SIZE}.json -md ./db_main/rocksdb/${SIZE}/ -sd $DATA_FOLDER -res ./bench/results/cores_16/disks_1/rocksdb/${SIZE}.json -th $THREADS -fl Init -ri 0 -rc 1
