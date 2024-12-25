3c58f537f1bcb6c7a162c35c0c9bb9af0abb802a
Shaleen Garg <shaleengarg.in@gmail.com>
2024-12-24 22:47:41 -0500

    load small changes

--- load.sh
+++ load.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+set -x
+
 SIZE="250GB"
 
 THREADS=16
-DATA_FOLDER="$HOME/slowssd/data/rocksdb/${SIZE}"
+DATA_FOLDER="$HOME/ssd/data/rocksdb/${SIZE}"
+METADATA_FOLDER="./db_main/rocksdb/${SIZE}"
 
-./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/${SIZE}.json -md ./db_main/rocksdb/${SIZE}/ -sd $DATA_FOLDER -res ./bench/results/cores_16/disks_1/rocksdb/${SIZE}.json -th $THREADS -fl Init -ri 0 -rc 1
+rm -rf $DATA_FOLDER  $METADATA_FOLDER
+
+./build_release/build/bin/ucsb_bench -db rocksdb  -cfg ./bench/configs/rocksdb/100GB.cfg -wl ./bench/workloads/${SIZE}.json -md $METADATA_FOLDER -sd $DATA_FOLDER -res ./bench/results/cores_16/disks_1/rocksdb/${SIZE}.json -th $THREADS -fl Init -ri 0 -rc 1
